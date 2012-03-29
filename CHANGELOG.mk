291b223c242949ed33cf0aab07937b4df45957c5
Johannes Röttger <johannes@roettger.us>
2012-03-29 18:59:46 +0200

    Fixes double typo.

--- CHANGELOG.mk
+++
@@ -1,4 +0,0 @@
-v0.1.0 [Mar 29 2012]
-- - - - - - - - - - -
-
-Initial import of normalize.css
--- README.mk
+++
@@ -1,9 +0,0 @@
-# normalize.css - Compass Plugin
-A Compass plugin of Nicolas Gallagher's normalize.css project.
-
-## License
-Public domain
-
-## Acknowledgements
-* [Normalize.css](https://github.com/necolas/normalize.css/) is a project by [Nicolas Gallagher](http://github.com/necolas) and [Jonathan Neal](http://github.com/jonathantneal).
-* [compass-normalize-plugin](https://github.com/jroettger/compass-normalize-plugin) is a [Compass](http://compass-style.org) plugin by [Johannes Röttger](http://github.com/jroettger).
